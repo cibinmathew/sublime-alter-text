8342e09e684117e00154e1f73f4c4b2b2345e5c1
Cibin Mathew <10793628+cibinmathew@users.noreply.github.com>
2023-06-14 14:18:44 +0200

    Added replacement arg (#2)

--- alter_text.py
+++ alter_text.py
@@ -3,9 +3,9 @@ import sublime_plugin
 
 
 class ReplaceTextWithCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
+    def run(self, edit, replacement=""):
         regions = self.view.sel()
         for region in regions:
             region_text = self.view.substr(region)
-            replacement_text = " " * len(region_text)
+            replacement_text = replacement * len(region_text)
             self.view.replace(edit, region, replacement_text)
